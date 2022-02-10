30b815e6dfda5fa753ae614c2a2e1304b59877ba
Arafa Mouad <90204890+MouadArafa@users.noreply.github.com>
2022-02-10 18:14:01 +0100

    Add files via upload

---
+++ App1/Page2.xaml.cs
@@ -0,0 +1,21 @@
+﻿using App1.MasterDetailPages;
+using System;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : Xamarin.Forms.TabbedPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            /*await Navigation.PushAsync(new Home());*/
+            App.Current.MainPage = new FlyoutPage1();
+        }
+    }
+}
